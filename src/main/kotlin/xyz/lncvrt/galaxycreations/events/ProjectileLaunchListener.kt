acf61e177d8ebb9f7f5cccb799f8030007567f97
Lncvrt <lncvrtreal@gmail.com>
2025-05-04 22:19:21 -0700

    Temp remove arrow excemption

--- src/main/kotlin/xyz/lncvrt/galaxycreations/events/ProjectileLaunchListener.kt
+++ src/main/kotlin/xyz/lncvrt/galaxycreations/events/ProjectileLaunchListener.kt
@@ -1,6 +1,5 @@
 package xyz.lncvrt.galaxycreations.events
 
-import org.bukkit.entity.EntityType
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.ProjectileLaunchEvent
@@ -8,7 +7,7 @@ import org.bukkit.event.entity.ProjectileLaunchEvent
 class ProjectileLaunchListener : Listener {
     @EventHandler
     fun onProjectileLaunchEvent(event: ProjectileLaunchEvent) {
-        if (event.entityType == EntityType.ARROW) return
+//        if (event.entityType == EntityType.ARROW) return
         event.isCancelled = true
     }
 }
